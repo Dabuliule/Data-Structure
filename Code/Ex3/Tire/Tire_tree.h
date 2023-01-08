738ab9da4187c37775638035b28a1e2d48176836
Dabuliule <614151250@qq.com>
2023-01-08 11:40:16 +0800

    2023/1/8

--- Code/Ex3/Tire/Tire_tree.h
+++ Code/Ex3/Tire/Tire_tree.h
@@ -4,7 +4,8 @@
 #include "LinkList.h"
 #include <functional>
 
-char to_lower(char &c) {
+char to_lower(char &c)
+{
     if (c >= 'A' && c <= 'Z')
         return c - 'A' + 'a';
     return c;
@@ -32,9 +33,9 @@ class Tire_Tree
 public:
     Tire_Tree();
     ~Tire_Tree();
-    void Insert(char *str, int row); // 插入单词
+    void Insert(char *str, int row);    // 插入单词
     void InsertRow(char *str, int row); // 插入一行
-    void Query(char *str);           // 查询单词
+    void Query(char *str);              // 查询单词
 
 private:
     Tire_Node *root;
@@ -74,12 +75,16 @@ void Tire_Tree::Insert(char *str, int row)
     root = temp;
 }
 
-void Tire_Tree::InsertRow(char *str, int row) {
+void Tire_Tree::InsertRow(char *str, int row)
+{
     char word[20];
-    for (int i = 0; i < strlen(str); ++i) {
-        if (isalpha(str[i])) {
+    for (int i = 0; i < strlen(str); ++i)
+    {
+        if (isalpha(str[i]))
+        {
             int j = 0;
-            while (i < strlen(str) && isalpha(str[i])) {
+            while (i < strlen(str) && isalpha(str[i]))
+            {
                 word[j++] = str[i++];
             }
             word[j] = '\0';
@@ -94,7 +99,8 @@ void Tire_Tree::Query(char *str)
     for (int i = 0; i < strlen(str); ++i)
     {
         int j = to_lower(str[i]) - 'a';
-        if (p->next[j] == NULL) {
+        if (p->next[j] == NULL)
+        {
             cout << "单词 " << str << " 出现次数为：" << p->count << endl;
             return;
         }
--- Code/Ex3/Tire/text.txt
+++ Code/Ex3/Tire/text.txt
@@ -1,25 +1,28 @@
 #include "Tire_tree.h"
 
-int main() {
+int main()
+{
     Tire_Tree tire;
 
     ifstream textfile, wordfile;
     textfile.open("text.txt", ios::in);
     wordfile.open("word.txt", ios::in);
     if (!textfile.is_open() || !wordfile.is_open())
-	{
-		cout << "读取文件失败" << endl;
+    {
+        cout << "读取文件失败" << endl;
         system("pause");
-		return 0;
-	}
+        return 0;
+    }
 
     char str[1024];
     int row = 0;
-    while (textfile.getline(str,sizeof(str))) {
+    while (textfile.getline(str, sizeof(str)))
+    {
         tire.InsertRow(str, ++row);
     }
 
-    while (wordfile >> str) {
+    while (wordfile >> str)
+    {
         tire.Query(str);
         cout << endl;
     }
