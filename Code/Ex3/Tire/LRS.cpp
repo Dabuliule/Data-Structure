7df82a1c366091f4fe486c9231e730db60ad6331
Dabuliule <614151250@qq.com>
2023-01-10 11:00:34 +0800

    2023/1/10

--- Code/Ex3/Tire/LRS.cpp
+++ Code/Ex3/Tire/LRS.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    Tire_Tree tire;
+    Tire_Tree tire; // 创建字典树
 
     ifstream textfile, wordfile;
-    textfile.open("text.txt", ios::in);
-    wordfile.open("word.txt", ios::in);
+    textfile.open("text.txt", ios::in); // 读取文章
+    wordfile.open("word.txt", ios::in); // 读取单词
     if (!textfile.is_open() || !wordfile.is_open())
     {
         cout << "读取文件失败" << endl;
@@ -16,18 +16,18 @@ int main()
 
     char str[1024];
     int row = 0;
-    while (textfile.getline(str, sizeof(str)))
+    while (textfile.getline(str, sizeof(str))) // 按行读取
     {
-        tire.InsertRow(str, ++row);
+        tire.InsertRow(str, ++row); // 将 str 中的每个单词都插入到字典树中
     }
 
-    while (wordfile >> str)
+    while (wordfile >> str) // 读取单词
     {
-        tire.Query(str);
+        tire.Query(str); // 查询单词
         cout << endl;
     }
 
-    textfile.close();
+    textfile.close(); // 关闭文件
     wordfile.close();
 
     system("pause");
--- Code/Ex3/Tire/Tire_tree.h
+++ Code/Ex3/Tire/Tire_tree.h
@@ -4,6 +4,8 @@
 #include "LinkList.h"
 #include <functional>
 
+class Tire_Tree;
+
 char to_lower(char &c)
 {
     if (c >= 'A' && c <= 'Z')
@@ -15,7 +17,9 @@ class Tire_Node
 {
 public:
     Tire_Node();
+    friend class Tire_Tree;
 
+private:
     int count;           // 统计单词个数
     LinkList<int> rows;  // 记录出现该单词的行号
     Tire_Node *next[26]; // 指向各个子树的指针
@@ -62,17 +66,17 @@ Tire_Tree::~Tire_Tree()
 
 void Tire_Tree::Insert(char *str, int row)
 {
-    Tire_Node *temp = root;
+    Tire_Node *temp = root; // 记录根节点
     for (int i = 0; i < strlen(str); ++i)
     {
-        int j = to_lower(str[i]) - 'a';
-        if (root->next[j] == NULL)
+        int j = to_lower(str[i]) - 'a'; // 把 str[i] 先转成小写，再获取位置
+        if (root->next[j] == NULL)      // 如果不存在到 str[i] 的路径，则创建
             root->next[j] = new Tire_Node();
-        root = root->next[j];
+        root = root->next[j]; // root 去到 str[i] 所在的子树
     }
-    root->count++;
-    root->rows.InsertElem(row);
-    root = temp;
+    root->count++;              // 文章中 str 出现的数量 +1
+    root->rows.InsertElem(row); // 记录单词 str 出现的行号
+    root = temp;                // root 回到根节点
 }
 
 void Tire_Tree::InsertRow(char *str, int row)
@@ -80,7 +84,7 @@ void Tire_Tree::InsertRow(char *str, int row)
     char word[20];
     for (int i = 0; i < strlen(str); ++i)
     {
-        if (isalpha(str[i]))
+        if (isalpha(str[i])) // 获取单词
         {
             int j = 0;
             while (i < strlen(str) && isalpha(str[i]))
@@ -88,7 +92,7 @@ void Tire_Tree::InsertRow(char *str, int row)
                 word[j++] = str[i++];
             }
             word[j] = '\0';
-            Insert(word, row);
+            Insert(word, row); // 插入单词
         }
     }
 }
@@ -101,7 +105,7 @@ void Tire_Tree::Query(char *str)
         int j = to_lower(str[i]) - 'a';
         if (p->next[j] == NULL)
         {
-            cout << "单词 " << str << " 出现次数为：" << p->count << endl;
+            cout << "单词 " << str << " 未出现" << endl;
             return;
         }
         p = p->next[j];
